d7d1ab7190c4c56e7715f9102bfc06751a69313a
Ragavi281202 <2002vgr@gmail.com>
2024-07-18 18:04:12 +0530

    deleted project

--- book_project/book_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.count_books, name='count_books'),
-    path('suggestions_search/', views.suggestions_search, name='suggestions_search'),
-    path('searching/', views.searching, name='searching')
-]
